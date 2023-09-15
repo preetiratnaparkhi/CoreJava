a83c24749b50235a18beec47abf8fb61b4b96597
preetiratnaparkhi <preetiratnaparkhi08@gmail.com>
2023-09-15 20:49:14 +0530

    1st commit

---
+++ src/Demo/First_Program.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+
+		int a = 10;
+
+		int b = 29;
+		System.out.println(a + b);
+
+	}
+//	 ctrl+shift+A : ctrl+shift+f 
+
+}
